d935d1ecd75a4e591426dcad46a06c7c45227729
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-18 16:11:00 -0300

    Spring Boot 2 Essentials 08 - Método GET parte 2

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,14 +1,19 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.services.AnimeService;
 import academy.devdojo.springboot2.util.Dateutil;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -20,20 +25,18 @@ import java.util.List;
 public class AnimeController {
 
     private final Dateutil dateutil;
+    private final AnimeService animeService;
 
 
     // s@RequestMapping(method = RequestMethod.GET, path = "list") localhost:8080/anime/list
-    @GetMapping(path = "list")
-    public List<Anime> list() {
+    @GetMapping
+    public ResponseEntity<List<Anime>> listAll() {
         log.info(dateutil.formatLocalDateTimeToDataBaseStryle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"),
-                new Anime("Bersek"));
+        return new ResponseEntity<>(animeService.listAll() , HttpStatus.OK);
     }
 
-    @GetMapping(path = "list2")
-    public List<Anime> list2() {
-        log.info(dateutil.formatLocalDateTimeToDataBaseStryle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"),
-                new Anime("One Piece2"));
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return  ResponseEntity.ok(animeService.findById(id));
     }
 }
---
+++ src/main/java/academy/devdojo/springboot2/services/AnimeService.java
@@ -0,0 +1,33 @@
+package academy.devdojo.springboot2.services;
+
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.IAnimeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+
+   private List<Anime> animes = List.of(new Anime(1L,"DBZ"),new Anime(2L,"Bersek"));
+
+    private IAnimeRepository animeRepository;
+
+    public List<Anime> listAll(){
+        return List.of(new Anime(1L,"DBZ"),
+                new Anime(2L,"Bersek"));
+    }
+
+    public Anime findById(Long id){
+    return  animes.stream()
+            .filter(anime -> anime.getId().equals(id))
+            .findFirst()
+            .orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime não encontrado "));
+
+    }
+}
