c9081d381e86213b95e66037ff8292700d04ba10
Gabryel C. <gabryelchianca8@gmail.com>
2026-08-19 13:26:18 -0300

    Spring Boot 2 Essentials 09 - Método Delete

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,14 +3,12 @@ package academy.devdojo.springboot2.controller;
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.services.AnimeService;
 import academy.devdojo.springboot2.util.Dateutil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -41,5 +39,11 @@ public class AnimeController {
     @ResponseStatus( HttpStatus.CREATED)
     public ResponseEntity<Anime> save(@RequestBody Anime anime){
         return ResponseEntity.ok(animeService.save(anime));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@RequestBody long id){
+        animeService.delete(id);
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
