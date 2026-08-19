b5ee96ab5d218039cc1469457ddb080bae9e68d2
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-19 15:43:35 -0300

    Spring Boot 2 Essentials 08 - Método Put

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 
 @RequiredArgsConstructor
@@ -45,5 +45,11 @@ public class AnimeController {
     public ResponseEntity<Void> delete(@RequestBody long id){
         animeService.delete(id);
       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime){
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
