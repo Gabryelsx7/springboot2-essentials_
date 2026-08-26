b930210ee4631b7d7a5058a5fdfb512b5a538ec6
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-26 14:35:27 -0300

    Spring Boot 2 Essentials 23 - Paginação

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -9,6 +9,8 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +28,9 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<List<Anime>> listAll() {
+    public ResponseEntity<Page<Anime>> listAll(Pageable pageable) {
         log.info(dateutil.formatLocalDateTimeToDataBaseStryle(LocalDateTime.now()));
-        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+        return new ResponseEntity<>(animeService.listAll(pageable), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
--- src/main/java/academy/devdojo/springboot2/services/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/services/AnimeService.java
@@ -1,6 +1,5 @@
 package academy.devdojo.springboot2.services;
 
-
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.dto.AnimePostDto;
 import academy.devdojo.springboot2.dto.AnimePutDto;
@@ -8,9 +7,9 @@ import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.IAnimeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,23 +22,29 @@ public class AnimeService {
     private static List<Anime> animes;
 
     static {
-        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Bersek")));
+        animes = new ArrayList<>(
+                List.of(
+                        new Anime(1L, "DBZ"),
+                        new Anime(2L, "Bersek")
+                )
+        );
     }
 
-
-    public List<Anime> listAll() {
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
 
     public Anime findByIdOrThorwBadRequestExcepetion(Long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new BadRequestException("Anime não encontrado "));
-
+                .orElseThrow(() ->
+                        new BadRequestException("Anime não encontrado ")
+                );
     }
 
     public Anime save(AnimePostDto animePostDto) {
-        return animeRepository.save( AnimeMapper.INSTANCE.toAnime(animePostDto));
-
+        return animeRepository.save(
+                AnimeMapper.INSTANCE.toAnime(animePostDto)
+        );
     }
 
     public void delete(Long id) {
@@ -47,8 +52,12 @@ public class AnimeService {
     }
 
     public void replace(AnimePutDto animePutDto) {
-        Anime savedAnime = findByIdOrThorwBadRequestExcepetion(animePutDto.getId());
-        Anime anime = AnimeMapper.INSTANCE.toAnime(animePutDto);
+        Anime savedAnime =
+                findByIdOrThorwBadRequestExcepetion(animePutDto.getId());
+
+        Anime anime =
+                AnimeMapper.INSTANCE.toAnime(animePutDto);
+
         anime.setId(savedAnime.getId());
 
         animeRepository.save(anime);
@@ -57,5 +66,4 @@ public class AnimeService {
     public List<Anime> findByName(String name) {
         return animeRepository.findByName(name);
     }
-
 }
