def48d54f2c46e478bb06e1186bc4f9f0efceb92
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-27 17:38:58 -0300

    Spring Boot 2 Essentials 25 - Sorting, Log SQL

--- src/main/java/academy/devdojo/springboot2/configurer/DevDojoWebMvnConfigurer.java
+++ src/main/java/academy/devdojo/springboot2/configurer/DevDojoWebMvnConfigurer.java
@@ -2,18 +2,23 @@ package academy.devdojo.springboot2.configurer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver; // IMPORTANTE: Versão correta para Spring MVC
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class DevDojoWebMvnConfigurer implements WebMvcConfigurer {
+public class DevDojoWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        ReactivePageableHandlerMethodArgumentResolver pageHandler = new ReactivePageableHandlerMethodArgumentResolver ();
-        pageHandler.setFallbackPageable(PageRequest.of(0,5));
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+
+        // Define que se o usuário não passar page/size na URL, o padrão será página 0 com 5 elementos
+        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+
+        // ADICIONADO: Obrigatório para o Spring registrar o resolvedor na aplicação
+        resolvers.add(pageHandler);
     }
 }
