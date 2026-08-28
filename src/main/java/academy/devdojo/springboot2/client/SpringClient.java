ddf67757d8206b1fc1e4561049c6c0113b806e89
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-28 17:45:38 -0300

    Spring Boot 2 Essentials 28 - RestTemplate POST

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -3,6 +3,7 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,9 +28,16 @@ public class SpringClient {
 
         ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null
                 , new ParameterizedTypeReference<List<Anime>>() {});
-
         log.info(exchange.getBody());
 
+//        Anime kingdom = Anime.builder().name("Kingdod").build();
+//        Anime kingdomSave = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+//        log.info("Saved anime {}", kingdomSave);
+
+        Anime samuraiChamplo = Anime.builder().name("samuraiChamplo").build();
+        ResponseEntity<Anime> samuraiChamploSave = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST,
+                new HttpEntity<>(samuraiChamplo), Anime.class);
+        log.info("Saved anime {}", samuraiChamploSave);
     }
 
 }
